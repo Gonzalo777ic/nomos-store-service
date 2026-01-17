8d2f337208eed9fe8d442998a473db7c87d53133
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-16 22:59:09 -0500

    implementación de entidad modelo sale return detail

---
+++ src/main/java/com/nomos/store/service/model/SaleReturnDetail.java
@@ -0,0 +1,36 @@
+package com.nomos.store.service.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "sale_return_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleReturnDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sale_return_id", nullable = false)
+    @JsonIgnore
+    @ToString.Exclude
+    private SaleReturn saleReturn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "original_sale_detail_id", nullable = false)
+    private SaleDetail originalSaleDetail;
+
+    @Column(nullable = false)
+    private Integer quantity;
+    @Column(name = "unit_price", nullable = false)
+    private Double unitPrice;
+
+    @Column(nullable = false)
+    private Double subtotal;
+}
