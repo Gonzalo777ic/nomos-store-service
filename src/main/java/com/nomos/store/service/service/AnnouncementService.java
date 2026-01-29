6011f6faa8679793341dbf524e324823164d8239
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-28 23:01:07 -0500

    creación de método getActiveAnnouncement

--- src/main/java/com/nomos/store/service/service/AnnouncementService.java
+++ src/main/java/com/nomos/store/service/service/AnnouncementService.java
@@ -1,4 +1,28 @@
 package com.nomos.store.service.service;
 
+import com.nomos.store.service.model.Announcement;
+import com.nomos.store.service.repository.AnnouncementRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AnnouncementService {
+
+    private final AnnouncementRepository repository;
+
+    /**
+     * Obtiene solo los anuncios que deben mostrarse actualmente al usuario.
+     * Filtra por estado activo y rango de fechas vigente.
+     */
+    public List<Announcement> getActiveAnnouncements() {
+        return repository.findActiveAnnouncements(LocalDateTime.now());
+    }
+
+
 }
