560d3031f024a15ce89a50110abfecee0ba69d10
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-29 00:27:34 -0500

    creación de endpoint para método getALl

--- src/main/java/com/nomos/store/service/controller/AnnouncementController.java
+++ src/main/java/com/nomos/store/service/controller/AnnouncementController.java
@@ -1,4 +1,28 @@
 package com.nomos.store.service.controller;
 
+import com.nomos.store.service.model.Announcement;
+import com.nomos.store.service.service.AnnouncementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/store/announcements")
+@RequiredArgsConstructor
 public class AnnouncementController {
+
+    private final AnnouncementService service;
+
+    /**
+     * Obtener todos los anuncios (Para gestión administrativa)
+     * GET /api/store/announcements
+     */
+    @GetMapping
+    public ResponseEntity<List<Announcement>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+
 }
