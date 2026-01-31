bd3bfc5ca7fcf2d33960dbaa8b5d5609e8e14967
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-30 21:38:53 -0500

    definción del atributo id

--- src/main/java/com/nomos/store/service/model/StoreSchedule.java
+++ src/main/java/com/nomos/store/service/model/StoreSchedule.java
@@ -1,4 +1,27 @@
 package com.nomos.store.service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "store_schedules", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "day_of_week")
+})
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
