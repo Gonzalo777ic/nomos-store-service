9a76d932613663e861d31ccb0f3b7a3380da693c
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-30 21:45:42 -0500

    creación de los controladores  del store schedule

---
+++ src/main/java/com/nomos/store/service/controller/StoreScheduleController.java
@@ -0,0 +1,30 @@
+package com.nomos.store.service.controller;
+
+import com.nomos.store.service.model.StoreSchedule;
+import com.nomos.store.service.service.StoreScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/store/schedule")
+@RequiredArgsConstructor
+public class StoreScheduleController {
+
+    private final StoreScheduleService service;
+
+    @GetMapping
+    public ResponseEntity<List<StoreSchedule>> getWeeklySchedule() {
+        return ResponseEntity.ok(service.getAllSchedules());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StoreSchedule> updateDaySchedule(
+            @PathVariable Long id,
+            @RequestBody StoreSchedule schedule) {
+        return ResponseEntity.ok(service.updateSchedule(id, schedule));
+    }
+}
