765023311c205f2889d07327285e01e5fdc0589c
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-30 21:49:16 -0500

    definidión de métodos abstracots de esta interface repository

--- src/main/java/com/nomos/store/service/repository/StoreScheduleExceptionRepository.java
+++ src/main/java/com/nomos/store/service/repository/StoreScheduleExceptionRepository.java
@@ -1,4 +1,20 @@
 package com.nomos.store.service.repository;
 
-public class StoreScheduleExceptionRepository {
+import com.nomos.store.service.model.StoreScheduleException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StoreScheduleExceptionRepository extends JpaRepository<StoreScheduleException, Long> {
+
+    Optional<StoreScheduleException> findByDate(LocalDate date);
+
+    List<StoreScheduleException> findByDateGreaterThanEqualOrderByDateAsc(LocalDate date);
+
+    boolean existsByDate(LocalDate date);
 }
