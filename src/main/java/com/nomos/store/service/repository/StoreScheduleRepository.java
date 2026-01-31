08dfce76a628da8cb53d760b3f2ff709b0cda536
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-30 21:41:52 -0500

    definición de métodos abstractoc del repositorio de store schedule

--- src/main/java/com/nomos/store/service/repository/StoreScheduleRepository.java
+++ src/main/java/com/nomos/store/service/repository/StoreScheduleRepository.java
@@ -1,4 +1,17 @@
 package com.nomos.store.service.repository;
 
-public class StoreScheduleRepository {
+import com.nomos.store.service.model.StoreSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.DayOfWeek;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface StoreScheduleRepository extends JpaRepository<StoreSchedule, Long> {
+
+    Optional<StoreSchedule> findByDayOfWeek(DayOfWeek dayOfWeek);
+
+    List<StoreSchedule> findAll();
 }
