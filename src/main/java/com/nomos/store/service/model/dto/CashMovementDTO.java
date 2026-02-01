4f8871f530cb71b9fc4fa056bb13a82edb4d8be9
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-02-01 01:33:07 -0500

    definición de atributos en cash movement dto

--- src/main/java/com/nomos/store/service/model/dto/CashMovementDTO.java
+++ src/main/java/com/nomos/store/service/model/dto/CashMovementDTO.java
@@ -1,4 +1,25 @@
 package com.nomos.store.service.model.dto;
 
+import com.nomos.store.service.model.CashMovement;
+import com.nomos.store.service.model.CashMovementStatus;
+import com.nomos.store.service.model.CashMovementType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class CashMovementDTO {
+    private Long id;
+    private LocalDateTime movementDate;
+    private CashMovementType type;
+    private Double amount;
+    private String paymentMethodName;
+    private String concept;
+    private String externalReference;
+    private CashMovementStatus status;
+    private Long saleId;
+
+
 }
