1d7cb08fc5465b02b01577ba4e014954ad0a245f
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-01-31 23:51:54 -0500

    definición de atributis id y movementDate

--- src/main/java/com/nomos/store/service/model/CashMovement.java
+++ src/main/java/com/nomos/store/service/model/CashMovement.java
@@ -1,4 +1,27 @@
 package com.nomos.store.service.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cash_movements")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CashMovement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime movementDate;
+
+
 }
